504be90a232e35119f432161e55c695ee5eda5dc
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2024-01-08 22:20:07 +0530

    Time: 7 ms (67.01%) | Memory: 11.2 MB (18.24%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-    vector<pair<int,int>>bits;
-    vector<int>ans;
-    int n=arr.size();
-    for(int i=0;i<n;i++)
-    {
-        int cnt=0;
-        int x=arr[i];
-        while(x){
-            x=x&(x-1);
-            cnt++;
+        vector<pair<int,int>>bits;
+        vector<int>v;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int count=0;
+            int x=arr[i];
+            while(x){
+                x=x&(x-1);
+                count++;
+            }
+            bits.push_back({count,arr[i]});
         }
-        bits.push_back({cnt,arr[i]});
-        
-    }
-    sort(bits.begin(),bits.end());
-    for(int i=0;i<n;i++)
-    {
-        ans.push_back(bits[i].second);
-    }
-    return ans;
+        sort(bits.begin(),bits.end());
+        for(int i=0;i<n;i++){
+            v.push_back(bits[i].second);
+        }
+        return v;
+  
     }
 };
