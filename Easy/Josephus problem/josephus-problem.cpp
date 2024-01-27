aebe4a81a0be30f0b730e544256e77a28910bc2d
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2024-01-27 18:46:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Josephus problem/josephus-problem.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int jos(vector<int>&v,int k,int c){
+        int n=v.size();
+        if(n==1){
+            return v[0];
+        }
+        c=(c+k)%n;
+        v.erase(v.begin()+c);
+        return jos(v,k,c);
+    }
+    int josephus(int n, int k)
+    {
+       //Your code here>
+       vector<int>v;
+       for(int i=1;i<=n;i++){
+           v.push_back(i);
+       }
+       int c=0;
+       int ans=jos(v,k-1,c);
+       return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
