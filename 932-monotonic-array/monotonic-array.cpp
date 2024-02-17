3f3b8317b52be3de115b212d83a711cb8e41d5e8
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2024-02-17 23:35:42 +0530

    Time: 117 ms (64.64%) | Memory: 98.8 MB (52.32%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    bool isAcending(vector<int>&nums){
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]>nums[i+1])
-                return false;             
-        }
-        return true;
-    }
-     bool isDecending(vector<int>&nums){
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]<nums[i+1])
-                return false;             
-        }
-        return true;
-    }
-
     bool isMonotonic(vector<int>& nums) {
-        return isAcending(nums)||isDecending(nums);
+        bool increasing=true;
+        bool decreasing=true;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i-1]<nums[i]){
+                decreasing=false;
+            }
+            else if(nums[i-1]>nums[i]){
+                increasing=false;
+            }
+        }
+        return increasing||decreasing;
     }
 };
