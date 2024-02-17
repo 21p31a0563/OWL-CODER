481a64d702fe46ca346b90ed2086ad4c807ad465
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2024-02-17 23:14:28 +0530

    Time: 74 ms (76.66%) | Memory: 57.4 MB (30.10%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,44 +1,25 @@
 class Solution {
-private:
-    // Function to check if it's possible to reach the building at index p
-    bool check(vector<int>& heights, int bricks, int ladders, int p) {
-        int rem_brick = bricks;
-       vector<int> need;
-
-        for (int i = 0; i < p; i++) {
-            if (heights[i + 1] > heights[i]) {
-                need.push_back(heights[i + 1] - heights[i]);
-            }
-        }
-
-        // Sort in ascending order
-      sort(need.begin(), need.end());
-      reverse(need.begin(),need.end());
-
-        while (!need.empty() && rem_brick >= need.back()) {
-            rem_brick -= need.back();
-            need.pop_back();
-        }
-
-        return ladders >= need.size();
-    }
-
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        int low = 0;
-        int high = n;
+        int i;
+        priority_queue<int,vector<int>>pq;
+        for(int i=0;i<heights.size()-1;i++)
+        {
+            int diff=heights[i+1]-heights[i];
+            if(diff>0){
+                //we need to use bricks or ladders
+                bricks-=diff;
+                pq.push(diff);
+                if(bricks<0){
+                    if(ladders<=0)return i;
+                    int getbackbricks=pq.top();
+                    bricks+=getbackbricks;
+                    ladders--;
+                    pq.pop();
 
-        // Binary search
-        while (low < high - 1) {
-            int mid = low + (high - low) / 2;
-
-            if (check(heights, bricks, ladders, mid))
-                low = mid;
-            else
-                high = mid;
+                }
+            }
         }
-
-        return low;
+        return i;
     }
 };
