b0dd1b466771cc6bcc0a4611100214a762c6a85a
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2024-02-22 11:50:56 +0530

    Time: 152 ms (40.13%) | Memory: 72.4 MB (21.11%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -4,10 +4,10 @@ public:
         vector<int>trusts(n+1);
         vector<int>trusted(n+1);
         for(auto it:trust){
-            trusts[it[0]]++;
-            trusted[it[1]]++;
+            ++trusts[it[0]];
+            ++trusted[it[1]];
         }
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=n;++i)
         {
             if(trusted[i]==n-1&&trusts[i]==0){
                 return i;
