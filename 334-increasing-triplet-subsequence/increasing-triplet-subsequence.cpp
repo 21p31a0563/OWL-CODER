13f2a9828cba4207bed4dceb711f302812c95d2d
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2024-03-07 22:07:07 +0530

    Time: 79 ms (85.40%) | Memory: 114.2 MB (49.55%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -3,7 +3,10 @@ public:
     bool increasingTriplet(vector<int>& nums) {
         int firstsmall=INT_MAX;
         int secondsmall=INT_MAX;
-        for(int i=0;i<nums.size();i++)
+        if (nums.size() < 3) {
+            return false;
+        }
+        for(int i=0;i<nums.size();++i)
         {
             if(nums[i]<=firstsmall)
             {
